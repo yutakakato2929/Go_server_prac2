f0836ada4524a5d9479e5e944e133438d659ba34
yutaka kato <yutakakato2929@gmail.com>
2022-06-23 23:23:26 +0900

    orenman

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Go_server_prac2
+
+go 1.18
--- pra.go
+++ pra.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"html/template"
+
+	//"io/ioutil"
 	"net/http"
 	"time"
 )
 
+/*
+type myMux struct{}
+
+func (m *myMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		defaultRoute(w, r)
+	case "/route1":
+		route1(w, r)
+	case "/route2":
+		route2(w, r)
+	case "/writeResponse":
+		writeResponse(w, r)
+	case "/login":
+		login(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+*/
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", defaultRoute)
-	mux.HandleFunc("/route1", route1)
-	mux.HandleFunc("/route2", route2)
-	mux.Handle("/anothermethod", handlerA{})
-	mux.HandleFunc("/lookRequest", lookRequest)
-	mux.HandleFunc("/writeResponse", writeResponse)
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/chat", chat)
+	mux.HandleFunc("/actioned", actioned)
+	//mux.Handle("/anothermethod", handlerA{})
+	//mux.HandleFunc("/lookRequest", lookRequest)
+	//mux.HandleFunc("/writeResponse", writeResponse)
+
+	//mux := &myMux{}
 	server := http.Server{
 		Addr:           ":8080",
 		Handler:        mux,
@@ -24,19 +49,39 @@ func main() {
 	}
 	server.ListenAndServe()
 }
-
+func chat(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	t := template.Must(template.ParseFiles("doc/chat.html"))
+	t.Execute(w, r.Form)
+}
+func actioned(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	t := template.Must(template.ParseFiles("doc/actioined.html"))
+	t.Execute(w, r.Form)
+}
 func defaultRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "It's defaultroute")
 }
-func route1(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "It's route1")
-}
-func route2(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "It's route2")
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		// 入力フォームを返す
+		t, _ := template.ParseFiles("doc/main.html")
+		t.Execute(w, nil)
+	}
+	if r.Method == "POST" {
+		// Requestを解析し入力情報を出力する。
+		/*r.ParseForm()
+		fmt.Println("name:", r.Form)
+		http.Redirect(w, r, "/login", 301)
+		*/
+		r.ParseForm()
+		t, _ := template.ParseFiles("doc/main.html")
+		t.Execute(w, r.Form)
+	}
 }
 
+/*
 type handlerA struct{}
-
 func (h handlerA) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "it's another method")
 }
@@ -70,3 +115,4 @@ func writeResponse(w http.ResponseWriter, r *http.Request) {
 	// http.Requestはio.Writerとして扱えるため、これでもOK
 	// fmt.Fprintln(w, "Bad request!")
 }
+*/
